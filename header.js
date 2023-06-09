bb74de4b7639b6bd5e02319efb61d75e0367803b
CB <cb@debian-BULLSEYE-live-builder-AMD64>
2023-06-09 20:51:47 +0200

    test javascript

---
+++ header.js
@@ -0,0 +1,13 @@
+document.write("\
+\
+<div id='header'> \
+	<p style='text-align:center;'>\
+		<a href='index.html'>Home</a> \
+		<a href='research.html'>Research</a>\
+		<a href='./docs/resume.pdf'>R&eacute;sum&eacute;</a>\
+		<a href='software.html'>Software</a>\
+		<a href='links.html'>Useful links</a>\
+	</p>\
+</div>\
+\
+");
